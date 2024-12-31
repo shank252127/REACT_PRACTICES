5c6e6ea971fc3a5f05e3c56f0fdf24a78f18f228
Shashank Singh <shashank252127@gmail.com>
2024-12-31 16:09:04 +0530

    package files

---
+++ React Igniting Our App 2/demo.js
@@ -0,0 +1,12 @@
+// npm is not short of node package manager. Its everything but not node package manager, its doesnt have fullform;
+// standard reporsitory of all the packages, or say library, its an package manager;
+
+// npm init to create package.json;
+
+// npm i -D parcel, parcel is a bundler which combines all the files (css,html,js) together. It bundles ur app so that it can be shipped to production;
+// -D above stands for the dev dependencies
+// React bydefault uses wepack bundler;
+
+// There are two type of dependencies -  1- dev depencencies; 2- Normal Dependencies
+
+// "^2.13.3" -
